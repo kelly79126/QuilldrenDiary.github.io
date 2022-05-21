2da01763a2efa9e88180264ed96e64a4ad21d819
kelly.chen <kelly.chen@beseye.com>
2022-05-21 19:28:25 +0800

    add swipe

--- js_Phone.js
+++ js_Phone.js
@@ -308,51 +308,51 @@ function previous_phone(){
 
 }
 //////////////////////////////////////////////////////////////////////
-// document.addEventListener('touchstart', handleTouchStart, false);        
-// document.addEventListener('touchmove', handleTouchMove, false);
+document.addEventListener('touchstart', handleTouchStart, false);        
+document.addEventListener('touchmove', handleTouchMove, false);
 
-// var xDown = null;                                                        
-// var yDown = null;
+var xDown = null;                                                        
+var yDown = null;
 
-// function getTouches(evt) {
-//   return evt.touches ||             // browser API
-//          evt.originalEvent.touches; // jQuery
-// }                                                     
+function getTouches(evt) {
+  return evt.touches ||             // browser API
+         evt.originalEvent.touches; // jQuery
+}                                                     
                                                                          
-// function handleTouchStart(evt) {
-//     const firstTouch = getTouches(evt)[0];                                      
-//     xDown = firstTouch.clientX;                                      
-//     yDown = firstTouch.clientY;                                      
-// };                                                
+function handleTouchStart(evt) {
+    const firstTouch = getTouches(evt)[0];                                      
+    xDown = firstTouch.clientX;                                      
+    yDown = firstTouch.clientY;                                      
+};                                                
                                                                          
-// function handleTouchMove(evt) {
-//     if ( ! xDown || ! yDown ) {
-//         return;
-//     }
+function handleTouchMove(evt) {
+    if ( ! xDown || ! yDown ) {
+        return;
+    }
 
-//     var xUp = evt.touches[0].clientX;                                    
-//     var yUp = evt.touches[0].clientY;
+    var xUp = evt.touches[0].clientX;                                    
+    var yUp = evt.touches[0].clientY;
 
-//     var xDiff = xDown - xUp;
-//     var yDiff = yDown - yUp;
+    var xDiff = xDown - xUp;
+    var yDiff = yDown - yUp;
                                                                          
-//     if ( Math.abs( xDiff ) > Math.abs( yDiff ) && touchable==1 ) {/*most significant*/
-//         if ( xDiff > 0 ) {
-//             next_phone();
-//         } else {
-//             previous_phone();
-//         }                       
-//     } else {
-//         if ( yDiff > 0 ) {
-//             /* down swipe */ 
-//         } else { 
-//             /* up swipe */
-//         }                                                                 
-//     }
-//     /* reset values */
-//     xDown = null;
-//     yDown = null;                                             
-// };
+    if ( Math.abs( xDiff ) > Math.abs( yDiff ) && touchable==1 ) {/*most significant*/
+        if ( xDiff > 0 ) {
+            next_phone();
+        } else {
+            previous_phone();
+        }                       
+    } else {
+        if ( yDiff > 0 ) {
+            /* down swipe */ 
+        } else { 
+            /* up swipe */
+        }                                                                 
+    }
+    /* reset values */
+    xDown = null;
+    yDown = null;                                             
+};
 
 // /* 禁止點兩下放大 */
 // $(function(){
