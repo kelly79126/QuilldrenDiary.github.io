6fb6d13e694f4102c268e585f14f7d989697dc09
kelly.chen <kelly79126@gmail.com>
2022-06-04 23:02:28 +0800

    add resource popup, add scroll bar, correct name

--- popup.js
+++ popup.js
@@ -7,7 +7,25 @@ function popup_credits() {
         items: {
             src: `<div class="credits-bg">
                     <div class="credits-container">
-                        <img src="./img/credits-popup.webp" class="credits-inside"/>
+                        <img src="./img/credits-popup.png" class="credits-inside"/>
+                    </div>
+                </div>`,
+            type: 'inline'
+        },
+        closeMarkup: '<button title="%title%" type="button" class="mfp-close" style="top: -35px; right: -10px"><img src="./img/pushpin-close.webp" style="width: 35px; height:auto; pointer-events:none;"></button>',
+    });
+}
+
+function popup_resource() {
+    $('#resources').on('click', function(event) {
+        event.stopPropagation();
+    });
+
+    $('#resources').magnificPopup({
+        items: {
+            src: `<div class="credits-bg">
+                    <div class="credits-container">
+                        <img src="./img/resource-popup.png" class="resource-inside"/>
                     </div>
                 </div>`,
             type: 'inline'
